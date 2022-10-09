b2853a3c60d1aa599a40b1152b965c98b29d728e
arkanalexei <arkanalexei@gmail.com>
2022-10-09 23:43:16 +0700

    added production

--- tk_pbp_d06/settings.py
+++ tk_pbp_d06/settings.py
@@ -20,6 +20,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PRODUCTION = os.getenv('DATABASE_URL') is not None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
