668838c97afffd772364acf3ea7f3a23a813a92b
Veivel <25278964+Veivel@users.noreply.github.com>
2022-10-27 16:30:59 +0700

    worked on modal and more deposit functionality

--- banksampah/forms.py
+++ banksampah/forms.py
@@ -2,11 +2,6 @@ from django.forms import Form, ModelForm
 from deposit.models import WasteDeposit
 from banksampah.models import News
 
-class DepositForm(ModelForm):
-    class Meta:
-        model = WasteDeposit
-        fields = ['type', 'description', 'mass']
-
 class NewsForm(ModelForm):
     class Meta:
         model = News
