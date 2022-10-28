93eef03ea16eff9660b9c50f119da2b6cf59504d
ajsebastians <abraham.j.sebastian@gmail.com>
2022-10-28 11:27:11 +0700

    progress about us tapi masih belum rapi

--- about/urls.py
+++ about/urls.py
@@ -4,5 +4,8 @@ from about.views import *
 app_name='about'
 
 urlpatterns = [
-     path('', about_us, name='about_us'),
+    path('', about_us, name='about_us'),
+    path('edit_feedback', edit_feedback, name='edit_feedback'),
+    path('delete_feedback', delete_feedback, name='delete_feedback'),
+    path('get-data/',get_data,name='get_data'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,12 +1,73 @@
-from django.shortcuts import render, redirect
-from django.forms import Form
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from about.forms import FeedbackForm
+from about.models import Feedback
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_http_methods
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from datetime import datetime as dt
+from django.http import JsonResponse
+from django.http import HttpResponse
+import json 
+
+@csrf_exempt
+def get_data(request):
+    all_feedback_groups = Feedback.objects.all()
+
+    all_feedback_list = []
+    for group in all_feedback_groups:
+        group_dict = {
+                "id" : int(group.pk),
+                "pengirim": group.pengirim,
+                "message":group.message,
+                "ratings":group.ratings,
+        }
+        all_feedback_list.append(group_dict)
+    
+    # Serialize query set to json
+    data = json.dumps(all_feedback_list)
+
+    return HttpResponse(data, content_type='application/json')
 
 def about_us(request):
-    return render(request, "about.html")
+    feedbacks = Feedback.objects.all().order_by("-created_at")[:4]
+    feedbacksAll = Feedback.objects.filter(user_id = request.user.id)
+    context = {'feedbacks':feedbacks,'feedbacksAll':feedbacksAll}
+
+    # create object of form
+    form = FeedbackForm(request.POST or None, request.FILES or None)
+      
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        data = form.save(commit=False) #bisa return object
+        try :
+            data.user_id = User.objects.get(id=request.user.id)
+        except:
+            return HttpResponseRedirect("/about/")
+
+        data.save()
+        if request.method == 'POST':
+          return HttpResponseRedirect("/about/")
+  
+    context['form']= form
+    return render(request, "about.html", context)
+
+def edit_feedback(request):
+  print(request)
+  if (request.method == "POST"):
+    id = request.POST.get("id")
+    message = request.POST.get("message")
+    ratings = request.POST.get("ratings")
+      
+    feedback = Feedback.objects.get(id=id)
+    feedback.message = message
+    feedback.ratings = ratings
+    feedback.save()
+
+  return HttpResponseRedirect("/about/")
+
+def delete_feedback(request):
+    if (request.method == "POST"):
+        id = request.POST.get("id")
+        Feedback.objects.filter(id=id).delete()
+    return HttpResponseRedirect("/about/")
