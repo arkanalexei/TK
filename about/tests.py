14982f11dcc9beb28a1cbe40b24cdfd5c85d6f4e
arkanalexei <arkanalexei@gmail.com>
2022-10-30 20:09:13 +0700

    test wangy

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from about.models import Feedback
 
 # Create your tests here.
+class AboutTest(TestCase):
+    def test_about(self):
+        c = Client()
+        response = c.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_feedback(self):
+        c = Client()
+        c.login(username="test", password="investasibodong")
+        Feedback.objects.create(pengirim="a", message="y", ratings=('5','5'))
--- banksampah/tests.py
+++ banksampah/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from deposit.views import *
+from banksampah.models import News
+
+
 
 # Create your tests here.
 class HomeTest(TestCase): 
     def setUp(self):
         # runs before every test function
-        
-        self.credentials = {} #TODO
-        user = User.objects.create_user()
+        user = User.objects.create_user(username='test', password='investasibodong')
+        userAdmin = User.objects.create_superuser('testAdmin', '', "senjatamakantuan")
+
     
     def test_home(self):
         c = Client()
@@ -19,10 +22,16 @@ class HomeTest(TestCase):
         c = Client()
         response = c.get("/news/json/")
         self.assertEqual(response.status_code, 200) 
-        
-    # def test_news_json_logged_in(self):
-    #     c = Client()
-    #     c.post("/login/", self.credentials)
-        
-    #     response = c.get("/news/json/")
-    #     self.assertEqual(response.status_code, 200) 
+
+    def test_news(self):
+        c = Client()
+        c.login(username="test", password="investasibodong")
+        response = c.get("/news/")
+        self.assertEqual(response.status_code, 200) 
+    
+    def test_news_read_more(self):
+        News.objects.create(title="jamet", description="khatulistiwa")
+        c = Client()
+        c.login(username="test", password="investasibodong")
+        response = c.get("/news/read/1/")
+        self.assertEqual(response.status_code, 200) 
