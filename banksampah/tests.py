d72490ba0498cb3a9341c68ebab846d6c85cddf5
Veivel <25278964+Veivel@users.noreply.github.com>
2022-10-30 15:12:44 +0700

    added unit test for banksampah app

--- banksampah/tests.py
+++ banksampah/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from deposit.views import *
 
 # Create your tests here.
+class HomeTest(TestCase): 
+    def setUp(self):
+        # runs before every test function
+        
+        self.credentials = {} #TODO
+        user = User.objects.create_user()
+    
+    def test_home(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_news_json(self):
+        c = Client()
+        response = c.get("/news/json/")
+        self.assertEqual(response.status_code, 200) 
+        
+    # def test_news_json_logged_in(self):
+    #     c = Client()
+    #     c.post("/login/", self.credentials)
+        
+    #     response = c.get("/news/json/")
+    #     self.assertEqual(response.status_code, 200) 
