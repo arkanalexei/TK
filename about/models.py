7cf313f64db1fd85bb070f412f8a3fa6d456d7ca
ajsebastians <abraham.j.sebastian@gmail.com>
2022-11-02 17:15:58 +0700

    benerin form feedback

--- about/models.py
+++ about/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Feedback(models.Model):
     pengirim = models.CharField(max_length=1000, default="")
-    message = models.TextField(max_length=None, default ="")
+    message = models.CharField(max_length=2000, default ="")
     ratings = models.CharField(max_length=1000, choices=[('1','1'),('2','2'),('3','3'),('4','4'),('5','5')])
     created_at = models.DateTimeField(auto_now_add=True,null=True) 
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, null=True)
